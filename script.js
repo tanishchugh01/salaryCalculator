538f0760a901ac26ec5f7d0f298b1a3165b24e92
tanishchugh01 <tanishchugh01@gmail.com>
2022-01-19 17:12:12 +0530

    initial version

---
+++ script.js
@@ -0,0 +1,18 @@
+var buton=document.getElementsByTagName('button')[0];
+buton.addEventListener('click',submit);
+
+function submit(){
+    var sa=document.getElementById("sa").value;
+    
+    var answer=document.getElementById('answer');
+    
+    
+}
+
+function getAnswer(sa)
+{
+    function hra()
+    {
+        return 
+    }
+}
