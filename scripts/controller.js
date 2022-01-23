7a9b9df10e40af1c6f3f1db65c96ea3cc9ecc69a
tanishchugh01 <tanishchugh01@gmail.com>
2022-01-24 01:12:53 +0530

    Added validate function

--- scripts/controller.js
+++ scripts/controller.js
@@ -7,9 +7,14 @@ function mymain()
     submit.addEventListener("click", changeValue);
 }
 
-function changeValue() {
+function changeValue() {    
     var mybs = document.getElementById("bs");
     allowences.bs = parseInt(mybs.value);
+
+    if (!validate())
+    {
+        return;
+    }
     
     var answer = document.querySelector("#answer");
     
@@ -20,8 +25,46 @@ function changeValue() {
             var ptag = document.createElement("p");
 
             ptag.innerHTML = `${key.toUpperCase()} is &#x20B9;${allowences[key]().toLocaleString('hi','in')}`;
-
+            
             answer.appendChild(ptag);
         }
+    }
+        
+        //DRY was voilated here
+        //
+        //document.getElementById("hra").innerText = allowances.hra();
+        // document.getElementById("da").innerText = allowances.da();
+        // document.getElementById("ta").innerText = allowances.ta();
+        // document.getElementById("ma").innerText = allowances.ma();
+        // document.getElementById("pf").innerText = allowances.pf();
+        // document.getElementById("gs").innerText = allowances.gs();
+        // document.getElementById("ns").innerText = allowances.ns();
+}
+
+function validate()
+{
+    var error=document.querySelector('#error');
+
+    if(!allowences.bs)
+    {
+        document.querySelector('#answer').innerHTML='';
+        
+        error.innerHTML='Please fill the Salary <i>properly</i>!';
+        
+        return false;
+    }
+    else if(allowences.bs<0)
+    {
+        document.querySelector('#answer').innerHTML='';
+        
+        error.innerHTML='Your Salary is too high ( <i>Just like you, may be :)</i> )';
+        
+        return false;
+    }
+    else
+    {
+        error.innerHTML='';
+    
+        return true;
     }
 }
